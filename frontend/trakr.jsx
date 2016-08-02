fc97cefd4b8cbcf59eadfe28ba22b325fbdb4443
Meagan <mfeniseycopes@gmail.com>
2016-08-02 10:56:29 -0400

    add static_pages_controller and static_pages#root view; add root to routes; ensure frontend entry file works;

---
+++ frontend/trakr.jsx
@@ -0,0 +1,16 @@
+// react
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// project
+
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    ReactDOM.render(
+      <h1>React lies here</h1>,
+       document.getElementById('react-root')
+     );
+   }
+)
