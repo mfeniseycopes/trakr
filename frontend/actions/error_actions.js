798e2be1a1582f77eed998368ba5d32466bbf812
Meagan <mfeniseycopes@gmail.com>
2016-08-03 10:10:45 -0400

    create error actions and constants

---
+++ frontend/actions/error_actions.js
@@ -0,0 +1,23 @@
+// project requires
+const AppDispatcher = require('../dispatchers/dispatcher');
+const ErrorConstants = require('../constants/error_constants');
+
+const ErrorActions = {
+
+  setErrors(form, errors) {
+    AppDispatcher.dispatch({
+      actionType: ErrorConstants.SET_ERRORS,
+      form: form,
+      errors: errors
+    });
+  },
+
+  clearErrors() {
+    AppDispatcher.dispatch({
+      actionType: ErrorConstants.CLEAR_ERRORS
+    });
+  }
+
+};
+
+module.exports =
---
+++ frontend/constants/error_constants.js
@@ -0,0 +1,8 @@
+const ErrorConstants = {
+
+  SET_ERRORS: "SET_ERRORS",
+  CLEAR_ERRORS: "CLEAR_ERRORS"
+
+};
+
+module.exports = ErrorConstants;
