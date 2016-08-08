94cbc51ae72211aad3ee6723b80b594c4f02ac57
Meagan <mfeniseycopes@gmail.com>
2016-08-08 14:48:47 -0400

    add date, time, duration fields

--- frontend/components/activities/activity_form.jsx
+++ frontend/components/activities/activity_form.jsx
@@ -36,8 +36,34 @@ const ActivityForm = React.createClass({
     this.setState({ activityType: e.target.value });
   },
 
+  changeDate(e) {
+    debugger
+    this.setState({ date: e.target.value });
+  },
+
   changeDescription(e) {
     this.setState({ description: e.target.value });
+  },
+
+  changeDurationHH(e) {
+    let duration = (e.target.value * 360 +
+      this.state.durationMM * 60 +
+      this.state.durationSS);
+    this.setState({ durationHH: e.target.value, speed: duration / this.state.distance });
+  },
+
+  changeDurationMM(e) {
+    let duration = (this.state.durationHH * 360 +
+      e.target.value * 60 +
+      this.state.durationSS);
+    this.setState({ durationMM: e.target.value, speed: duration / this.state.distance });
+  },
+
+  changeDurationSS(e) {
+    let duration = (this.state.durationHH * 360 +
+      this.state.durationMM * 60 +
+      e.target.value);
+    this.setState({ durationSS: e.target.value, speed: duration / this.state.distance });
   },
 
   changeFile(e) {
@@ -52,6 +78,11 @@ const ActivityForm = React.createClass({
     }
   },
 
+  changeStart(e) {
+    debugger
+    this.setState({ start: e.target.value });
+  },
+
   changeTitle(e) {
     this.setState({ title: e.target.value });
   },
@@ -60,7 +91,8 @@ const ActivityForm = React.createClass({
     this.errorListener = ErrorStore.addListener(this.addErrors);
 
     // populate activityType dropdown
-    this.activityTypeListener = ActivityTypeStore.addListener(this.addActivityTypes);
+    this.activityTypeListener =
+      ActivityTypeStore.addListener(this.addActivityTypes);
     ActivityTypeActions.getActivityTypes();
 
     // get route info from prepopulated activity
@@ -75,16 +107,35 @@ const ActivityForm = React.createClass({
     this.activityTypeListener.remove();
   },
 
+  duration() {
+    return this.state.durationHH * 360 +
+      this.state.durationMM * 60 +
+      this.state.durationSS;
+  },
+
   getInitialState() {
+
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = ("00" + date.getMonth()).slice(-2);
+    let day = ("00" + date.getDate()).slice(-2);
+    let hh = ("00" + date.getHours()).slice(-2);
+    let mm = ("00" + date.getMinutes()).slice(-2);
+
     let state = {
-      activityType: null,
+      activityType: 1,
+      date: `${year}-${month}-${day}`,
       description: "",
       distance: 0,
-      duration: 0,
+      durationHH: "",
+      durationMM: "",
+      durationSS: "",
       encPolyline: "",
       gpxFile: null,
-      title: "",
+      start: `${hh}:${mm}`,
+      title: "My Acivity",
       route: null,
+      speed: 0,
       activityTypes: [],
       errors: []
     };
@@ -99,13 +150,7 @@ const ActivityForm = React.createClass({
       // state.gpxFile = ??
     }
     else {
-      activity = {
-        activityType: null,
-        description: "",
-        distance: 0,
-        gpxFile: null,
-        title: ""
-      };
+
     }
 
     return state;
@@ -113,25 +158,34 @@ const ActivityForm = React.createClass({
 
   handleSubmit() {
 
+    // format date
+    let date = new Date(this.state.date + " " + this.state.start);
+
     let activity;
+
     if (this.state.gpxFile) {
       activity = new FormData();
       formData.append("activity[gpx]", this.state.gpxFile);
       formData.append("activity[activity_type_id]", this.state.activityType);
       formData.append("activity[description]", this.state.description);
+      formData.append("activity[duration]", this.duration());
       formData.append("activity[distance]", this.state.distance);
       formData.append("activity[enc_polyline]", this.state.encPolyline);
       formData.append("activity[title]", this.state.title);
     } else if (this.state.route) {
       activity = {
         activity_type_id: this.state.activityType,
+        date: date,
         description: this.state.description,
         distance: this.state.distance,
+        duration: this.duration(),
         enc_polyline: this.state.encPolyline,
         route: this.state.route,
         title: this.state.title
       };
     }
+
+    debugger
 
     ActivityActions.createActivity(activity);
   },
@@ -144,11 +198,11 @@ const ActivityForm = React.createClass({
 
     return(
       <div>
-        <p>{ this.state.distance }</p>
+        <p>{ this.state.distance } mi</p>
 
         <img className="mini-map" src={ this.miniMapUrl() } />
 
-        <form onSubmit={this.handleSubmit}>
+        <form className="activity-form" onSubmit={this.handleSubmit}>
 
           <select name="Type" onChange={this.changeActivityType}>
             <option value="" >--Type--</option>
@@ -157,15 +211,34 @@ const ActivityForm = React.createClass({
 
           <input type="text"
             onChange={this.changeTitle}
-            value={this.state.title} />
+            value={this.state.title}
+            placeholder="Title" />
+
+          <input type="date"
+            onChange={this.changeDate}
+            value={this.state.date}/>
 
           <input type="time"
-            onChange={this.changeDuration}
-            value={this.state.duration} />
+            onChange={this.changeStart}
+            value={this.state.start} />
+
+          <input type="num"
+            onChange={this.changeDurationHH}
+            value={this.state.durationHH}
+            placeholder="HH" />
+          <input type="num"
+            onChange={this.changeDurationMM}
+            value={this.state.durationMM}
+            placeholder="MM" />
+          <input type="num"
+            onChange={this.changeDurationSS}
+            value={this.state.durationSS}
+            placeholder="SS" />
 
           <textarea
             onChange={this.changeDescription}
-            value={this.state.description} />
+            value={this.state.description}
+            placeholder="Description" />
 
           <input type="file"
             onChange={this.changeFile} />
@@ -178,6 +251,40 @@ const ActivityForm = React.createClass({
   }
 
 });
+
+let _HH = (duration) => {
+  if (duration) {
+    return ("00" + Math.floor(duration / 360) % 60).slice (-2);
+  } else {
+    return "";
+  }
+};
+let _MM = (duration) => {
+  if (duration) {
+    return ("00" + Math.floor(duration / 60) % 60).slice (-2);
+  } else {
+    return "";
+  }
+};
+let _SS = (duration) => {
+  if (duration) {
+    return ("00" + duration % 60).slice (-2);
+  } else {
+    return "";
+  }
+};
+
+let _formatDuration = (duration) => {
+  let ss = ("00" + (duration % 60)).slice (-2);
+  duration = Math.floor(duration / 60);
+
+  let mm = ("00" + (duration % 60)).slice (-2);
+  duration = Math.floor(duration / 60);
+
+  let hh = ("00" + (duration % 60)).slice (-2);
+
+  return `${hh}:${mm}:${ss}`;
+};
 
 
 module.exports = ActivityForm;
